32819ff60273ac7326b6ac2ae7a4bd13046c8363
Daniel Meireles <daniel.meireles@outlook.com>
2020-08-05 20:49:07 -0300

    Add cors

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
 import 'reflect-metadata';
 
+import cors from 'cors';
+
 import express from 'express';
 import routes from './routes';
 
@@ -9,6 +11,7 @@ const port = 3333;
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
